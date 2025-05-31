eb3b7c0ad9c03a470d79491e09be45b7dbb0a9f7
lsantoss2 <lsantoss2@miumg.edu.gt>
2025-05-31 02:37:35 -0600

    cambio de index

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-    base: './', 
-=======
   base: './', 
->>>>>>> e6bb6b950718144fbca0138b0fcb75fdc845de03
 })
